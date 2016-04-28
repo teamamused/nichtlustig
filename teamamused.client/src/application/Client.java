4bd9008b92d02be1f80bc20e2d0f2d30e237a634
DanielHirsbrunner <dhirsbrunner@gmail.com>
2016-04-28 22:29:32 +0200

    + Initiales Checkin der Struktur

---
+++ teamamused.client/src/application/Client.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+
+public class Client extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			Label l =new Label();
+			l.setText("Dies ist der Client!");
+			root.setCenter(l);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ teamamused.common/src/teamamused/common/interfaces/IGameCard.java
@@ -0,0 +1,5 @@
+package teamamused.common.interfaces;
+
+public interface IGameCard {
+
+}
