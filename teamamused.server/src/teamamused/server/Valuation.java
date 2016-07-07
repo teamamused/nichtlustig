79deb9348513659366d35425b206e30b8757b267
Maja Velickovic <maja.velickovic@students.fhnw.ch>
2016-07-07 19:16:17 +0200

    JavaDOC ergänzt, damit diese komplett ist

--- teamamused.server/src/teamamused/server/Valuation.java
+++ teamamused.server/src/teamamused/server/Valuation.java
@@ -16,6 +16,7 @@ public class Valuation {
 	/**
 	 * Wertet die Karten der Spieler aus und wertet sie anschliessend gemäss dem
 	 * Wert des pinken Würfels.
+	 * @param board Board von BoardManager übergeben
 	 */
 	public void valuate(BoardManager board){
 		valuationValue = CubeManager.getInstance().getCurrentPinkCubeValue().FaceValue;
