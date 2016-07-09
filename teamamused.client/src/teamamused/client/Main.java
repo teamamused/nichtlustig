c960ada91cac7a4b4a8a41711d74a491634a1226
Michelle <michelle.widmer@students.fhnw.ch>
2016-07-09 15:45:57 +0200

    Main-Klasse um GameBoard-Initialisierung ergänzt

--- teamamused.client/src/teamamused/client/Main.java
+++ teamamused.client/src/teamamused/client/Main.java
@@ -1,41 +1,62 @@
 package teamamused.client;
 	
-import teamamused.client.connect.Client;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import teamamused.client.connect.Client;
+import teamamused.client.gui.GameBoardController;
+import teamamused.client.gui.GameBoardModel;
+import teamamused.client.gui.GameBoardView;
+import teamamused.common.ServiceLocator;
 
 
 public class Main extends Application {
 	@Override
+	
+	//TODO: Löschen, wenn nicht mehr benötigt
+//	public void start(Stage primaryStage) {
+//		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			Label l =new Label();
+//			l.setText("Dies ist der Client!");
+//			Button b = new Button("Start");
+//			b.setOnAction(new EventHandler<ActionEvent>() {
+//				
+//				@Override
+//				public void handle(ActionEvent event) {
+//					Client.getInstance().startGame();
+//					
+//				}
+//			});
+//			root.setCenter(l);
+//			root.setBottom(b);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			Label l =new Label();
-			l.setText("Dies ist der Client!");
-			Button b = new Button("Start");
-			b.setOnAction(new EventHandler<ActionEvent>() {
-				
-				@Override
-				public void handle(ActionEvent event) {
-					Client.getInstance().startGame();
-					
-				}
-			});
-			root.setCenter(l);
-			root.setBottom(b);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
+		//TODO: Sandra -> Start-Button muss Client.getInstance().startGame() aufrufen
+		
+		// Spiel starten
+//		Client.getInstance().startGame();
+        ServiceLocator.getInstance().getLogger().info("Gehe zu GUI");
+		
+        // Initialisierung des GameBoard-GUI
+        GameBoardModel model = new GameBoardModel();
+        GameBoardView view = new GameBoardView(primaryStage, model);
+        new GameBoardController(model, view);
+        
+        //defaultLogger.info("Starte View");
+        // gui anzeigen
+        view.start();
+        
+
+        ServiceLocator.getInstance().getLogger().info("Client ist verbunden!");
 	}
 	
 	public static void main(String[] args) {
