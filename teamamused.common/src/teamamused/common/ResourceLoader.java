c4f23ab7562b5bfc3586f6486e24626c73e06244
DanielHirsbrunner <dhirsbrunner@gmail.com>
2016-07-11 20:43:46 +0200

    Image Path mit relativem Pfad

--- teamamused.common/src/teamamused/common/ResourceLoader.java
+++ teamamused.common/src/teamamused/common/ResourceLoader.java
@@ -16,26 +16,27 @@ import javafx.scene.image.Image;
  */
 public class ResourceLoader {
 
-    /**
-     * Lädt ein Bild vom Dateisystem
-     * 
-     * @param imageName
-     *            Name des Bildes (samt endung)
-     * @return Image Objekt des Bildes
-     * @throws FileNotFoundException Exception falls Datei nicht gefunden
-     */
-    public static Image getImage(String imageName) throws FileNotFoundException {
-	File f = new File(ServiceLocator.getInstance().getSettings().getSetting(Setting.ImagePath)+ "/" + imageName);
-	if (!f.exists()) {
-	    ServiceLocator.getInstance().getLogger().warning(f.getAbsolutePath() + " - Konnte nicht gefunden werden");
-	    throw new FileNotFoundException("Das Bild " + imageName + " konnte nicht gefunden werden!");
+	/**
+	 * Lädt ein Bild vom Dateisystem
+	 * 
+	 * @param imageName
+	 *            Name des Bildes (samt endung)
+	 * @return Image Objekt des Bildes
+	 * @throws FileNotFoundException
+	 *             Exception falls Datei nicht gefunden
+	 */
+	public static Image getImage(String imageName) throws FileNotFoundException {
+		String imagePath = ServiceLocator.getInstance().getSettings().getSetting(Setting.ImagePath);
+		File f = new File(imagePath+ "/" + imageName);
+		if (!f.exists()) {
+			ServiceLocator.getInstance().getLogger().warning(f.getAbsolutePath() + " - Konnte nicht gefunden werden");
+			throw new FileNotFoundException("Das Bild " + imagePath+ "/" + imageName + " konnte nicht gefunden werden!");
+		}
+		try {
+			return new Image("file:" + f.getAbsolutePath());
+		} catch (Exception e) {
+			ServiceLocator.getInstance().getLogger().log(Level.SEVERE, e.getMessage(), e);
+		}
+		return null;
 	}
-	try {
-	    return new Image("file:" + f.getAbsolutePath());
-	} catch (Exception e) {
-	    ServiceLocator.getInstance().getLogger().log(Level.SEVERE, e.getMessage(), e);
-	}
-	return null;
-    }
-    
 }
