eb44e58ee944dd6240a8b58acbc8fbe1eae69096
DanielHirsbrunner <dhirsbrunner@gmail.com>
2016-07-11 20:02:04 +0200

    Main Version von Sandy

--- teamamused.client/src/teamamused/client/Main.java
+++ teamamused.client/src/teamamused/client/Main.java
@@ -1,62 +1,64 @@
 package teamamused.client;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import teamamused.client.gui.GameBoardController;
-import teamamused.client.gui.GameBoardModel;
-import teamamused.client.gui.GameBoardView;
-import teamamused.client.libs.Client;
-import teamamused.common.ServiceLocator;
 
+import teamamused.client.gui.LogInController;
+import teamamused.client.gui.LogInModel;
+import teamamused.client.gui.LogInView;
+import teamamused.client.gui.WelcomeController;
+import teamamused.client.gui.WelcomeModel;
+import teamamused.client.gui.WelcomeView;
+import teamamused.client.gui.splashscreen.Splash_Controller;
+import teamamused.client.gui.splashscreen.Splash_Model;
+import teamamused.client.gui.splashscreen.Splash_View;
+import javafx.application.Application;
+
+import javafx.stage.Stage;
 
 public class Main extends Application {
+
+	private Splash_View splashView;
+	private LogInView logInView;
+	private WelcomeView welcomeView;
+	private static Main instance = null;
+
 	@Override
-	
-	//TODO: Löschen, wenn nicht mehr benötigt
-//	public void start(Stage primaryStage) {
-//		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			Label l =new Label();
-//			l.setText("Dies ist der Client!");
-//			Button b = new Button("Start");
-//			b.setOnAction(new EventHandler<ActionEvent>() {
-//				
-//				@Override
-//				public void handle(ActionEvent event) {
-//					Client.getInstance().startGame();
-//					
-//				}
-//			});
-//			root.setCenter(l);
-//			root.setBottom(b);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-
 	public void start(Stage primaryStage) {
+		Main.instance = this;
+		// Create and display the splash screen and model
+		Splash_Model splashModel = new Splash_Model();
+		splashView = new Splash_View(primaryStage, splashModel);
+		new Splash_Controller(this, splashModel, splashView);
+		splashView.start();
 
-		//TODO: Sandra -> Start-Button muss Client.getInstance().startGame() aufrufen
-		
-		// Spiel starten
-//		Client.getInstance().startGame();
-        ServiceLocator.getInstance().getLogger().info("Gehe zu GUI");
-		
-        // Initialisierung des GameBoard-GUI
-        GameBoardModel model = new GameBoardModel();
-        GameBoardView view = new GameBoardView(primaryStage, model);
-        new GameBoardController(model, view);
-        
-        //defaultLogger.info("Starte View");
-        // gui anzeigen
-        view.start();
-        
+		// Display the splash screen and begin the initialization
+		splashModel.initialize();
+	}
 
-        ServiceLocator.getInstance().getLogger().info("Client ist verbunden!");
+	public void startLogIn() {
+
+		Stage logInStage = new Stage();
+
+		LogInModel model = new LogInModel();
+		logInView = new LogInView(logInStage, model);
+		new LogInController(model, logInView);
+		splashView.stop();
+		splashView = null;
+		logInView.start();
+	}
+
+	public void startWelcome() {
+
+		Stage welcomeStage = new Stage();
+
+		WelcomeModel model = new WelcomeModel();
+		welcomeView = new WelcomeView(welcomeStage, model);
+		new WelcomeController(model, welcomeView);
+		logInView.stop();
+		logInView = null;
+		welcomeView.start();
+	}
+
+	public static Main getInstance() {
+		return instance;
 	}
 	
 	public static void main(String[] args) {
