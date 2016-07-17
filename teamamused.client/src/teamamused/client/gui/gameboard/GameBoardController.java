cd0af7bb1d254410ed3970c2977d770afd3a8022
Michelle <michelle.widmer@students.fhnw.ch>
2016-07-17 14:01:05 +0200

    ActionEvent auf dem Hyperlink registriert, um Browser zu öffnen

--- teamamused.client/src/teamamused/client/gui/gameboard/GameBoardController.java
+++ teamamused.client/src/teamamused/client/gui/gameboard/GameBoardController.java
@@ -1,12 +1,23 @@
 package teamamused.client.gui.gameboard;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import teamamused.common.ServiceLocator;
 import teamamused.common.gui.AbstractController;
 
 public class GameBoardController extends AbstractController<GameBoardModel, GameBoardView> {
 
 	public GameBoardController(GameBoardModel model, GameBoardView view) {
 		super(model, view);
-		// TODO Auto-generated constructor stub
+
+		// Auf dem Hyperlink wird ein ActionEvent registiert, welches den
+		// Browser öffnet und das entsprechende HTML-Dokument zurückgibt
+		view.linkAnleitung.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				ServiceLocator.getInstance().getHostServices().showDocument(view.linkAnleitung.getText());
+			}
+		});
 	}
 
 }
