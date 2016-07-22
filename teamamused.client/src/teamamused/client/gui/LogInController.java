4eb30b52b62d6a0505ffe5498bd5eb5c9cc7a3e0
Sandra <Sandra@10.59.52.194>
2016-07-22 12:38:51 +0200

    Logik-Teil von Dani in LogInController und RegisterController eingebaut

--- teamamused.client/src/teamamused/client/gui/LogInController.java
+++ teamamused.client/src/teamamused/client/gui/LogInController.java
@@ -1,7 +1,11 @@
 package teamamused.client.gui;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.ButtonBase;
 import teamamused.client.Main;
+import teamamused.client.libs.Client;
+import teamamused.common.ServiceLocator;
 import teamamused.common.gui.AbstractController;
 
 public class LogInController extends AbstractController<LogInModel, LogInView> {
@@ -11,6 +15,25 @@ public class LogInController extends AbstractController<LogInModel, LogInView> {
 		
 		view.btnLogin.setOnAction((ActionEvent e) -> {
 			Main.getInstance().startWelcome();
+		});
+		
+		view.btnLogin.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Client.getInstance().logIn(view.textUser.getText(),view.textPassword.getText());
+			}
+		});
+		
+		view.btnConnectServer.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				boolean erfolgreich = Client.getInstance().ConnectToServer(view.textServer.getText(), view.textUser.getText(), Integer.parseInt(view.textPort.getText()));
+				if (erfolgreich) {
+					ServiceLocator.getInstance().getLogger().info("Erfolgreich zum Server verbunden");
+				} else {
+					ServiceLocator.getInstance().getLogger().info("Verbindung zum Server konnte nicht hergestellt werden.");
+				}
+			}
 		});
 		
 		view.linkReg.setOnAction((ActionEvent e) -> {
