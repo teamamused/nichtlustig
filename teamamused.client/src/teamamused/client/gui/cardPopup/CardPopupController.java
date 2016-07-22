38b8bb3e37951f053410a29cc91cb7c707737e14
Michelle <michelle.widmer@students.fhnw.ch>
2016-07-22 15:20:07 +0200

    Controller für CardPopup begonnen

--- teamamused.client/src/teamamused/client/gui/cardPopup/CardPopupController.java
+++ teamamused.client/src/teamamused/client/gui/cardPopup/CardPopupController.java
@@ -1,11 +1,21 @@
 package teamamused.client.gui.cardPopup;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import teamamused.common.gui.AbstractController;
 
 public class CardPopupController extends AbstractController<CardPopupModel, CardPopupView> {
 
 	public CardPopupController(CardPopupModel model, CardPopupView view) {
 		super(model, view);
+		
+		// Der Klick auf den Schliessen-Button schliesst das Fenster
+		view.btnClose.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				view.stop();
+			}
+		});
 
 	}
 }
