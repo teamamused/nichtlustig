4e6b01c228a0dda14edee7b021248420e771222a
Sandra <Sandra@Sandra-PC.fritz.box>
2016-08-11 22:14:24 +0200

    Fehlermeldung bei ungleichen Passwörtern

--- teamamused.client/src/teamamused/client/gui/register/RegisterController.java
+++ teamamused.client/src/teamamused/client/gui/register/RegisterController.java
@@ -16,15 +16,16 @@ import teamamused.common.ServiceLocator;
 import teamamused.common.gui.AbstractController;
 import teamamused.common.interfaces.IPlayer;
 
-public class RegisterController extends AbstractController<RegisterModel, RegisterView> implements IClientListener {
+public class RegisterController extends AbstractController<RegisterModel, RegisterView>implements IClientListener {
 
 	public RegisterController(RegisterModel model, RegisterView view) {
 		super(model, view);
 
 		// Beim Client registrieren
 		Client.getInstance().registerGui(this);
-		
-		// Wenn Zurück-Button durch Benutzer angeklickt wird, wird er zurück auf die Loginseite gebracht
+
+		// Wenn Zurück-Button durch Benutzer angeklickt wird, wird er zurück auf
+		// die Loginseite gebracht
 		view.btnBack.setOnAction((ActionEvent e) -> {
 			Main.getInstance().startLogIn(this.view);
 		});
@@ -43,27 +44,35 @@ public class RegisterController extends AbstractController<RegisterModel, Regist
 		view.btnRegister.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				Client.getInstance().registerPlayer(view.textRegUser.getText(), view.password.getText());
+				if (view.password == view.password2) {
+					Client.getInstance().registerPlayer(view.textRegUser.getText(), view.password.getText());
+				} else {
+					Platform.runLater(() -> {
+						Alert alert = new Alert(AlertType.ERROR, "Die beiden Passwörter sind nicht identisch");
+						alert.show();
+					});
+				}
 			}
 		});
-
 	}
-	
-	//Methode leitet Benutzer auf die Welcome-Seite weiter, wenn Registrierung erfolgreich
+
+	// Methode leitet Benutzer auf die Welcome-Seite weiter, wenn Registrierung
+	// erfolgreich
 	@Override
 	public void onRegisterSuccessful(IPlayer player) {
 		Platform.runLater(() -> {
-		Main.getInstance().startWelcome(this.view);
+			Main.getInstance().startWelcome(this.view);
 		});
 		ServiceLocator.getInstance().getLogger().info("Der Benutzer wurde erfolgreich registriert");
 	}
-	
-	//Methode gibt dem Benutzer eine Fehlermeldung zurück, wenn Registrierung nicht korrekt funktioniert hat
+
+	// Methode gibt dem Benutzer eine Fehlermeldung zurück, wenn Registrierung
+	// nicht korrekt funktioniert hat
 	@Override
 	public void onRegisterFailed(String msg) {
 		Platform.runLater(() -> {
-		Alert alert = new Alert(AlertType.ERROR,"Registrierung fehlgeschlagen" + msg);
-		alert.show();
+			Alert alert = new Alert(AlertType.ERROR, "Registrierung fehlgeschlagen" + msg);
+			alert.show();
 		});
 	}
 
